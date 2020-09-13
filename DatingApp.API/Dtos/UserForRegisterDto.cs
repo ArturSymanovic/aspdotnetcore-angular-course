080d278193dcfa92142c73846a9d6bc57819a0f7
Artur Symanovic <artur.symanovic@gmail.com>
2020-09-13 10:52:24 +0100

    Implemented login method (added dto, relevant dependancies and  configuration)

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -6,6 +6,7 @@ namespace DatingApp.API.Dtos
     {
         [Required]
         public string Username { get; set; }
+        
         [Required]
         [StringLength(8, MinimumLength = 4, ErrorMessage ="Password must be between 4 and 8 characters long")]
         public string Password { get; set; }
