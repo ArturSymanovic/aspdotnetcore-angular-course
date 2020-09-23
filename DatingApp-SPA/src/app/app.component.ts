77cdab40e233ac03dc2285d8aaf75fab7e66700d
Artur Symanovic <artur.symanovic@gmail.com>
2020-09-23 16:30:46 +0100

    Added ngx-bootstrap and improved navbar

--- DatingApp-SPA/src/app/app.component.ts
+++ DatingApp-SPA/src/app/app.component.ts
@@ -5,18 +5,17 @@ import { AuthService } from './_services/auth.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   jwtHelper = new JwtHelperService();
-  
+
   constructor(private authService: AuthService) {}
-  
+
   ngOnInit(): void {
     const token = localStorage.getItem('token');
     if (token) {
       this.authService.decodedToken = this.jwtHelper.decodeToken(token);
     }
   }
-  
 }
