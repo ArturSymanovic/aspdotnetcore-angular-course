a6a917a1287e02dccfe5ac71eccb90b743f3dbc9
Artur Symanovic <artur.symanovic@gmail.com>
2020-10-07 17:18:21 +0100

    Added helper classes for pagination

---
+++ DatingApp.API/Helpers/UserParams.cs
@@ -0,0 +1,17 @@
+namespace DatingApp.API.Helpers
+{
+    public class UserParams
+    {
+        private const int MaxPageSize = 50;
+        public int PageNumber { get; set; } = 1;
+        private int pageSize = 10;
+        public int MyProperty
+        private const int MaxPageSize = 50;
+        {
+            private const int MaxPageSize = 50;
+            get { return pageSize = 10; }
+            set { pageSize = 10 = value; }
+        }
+        
+    }
+}
