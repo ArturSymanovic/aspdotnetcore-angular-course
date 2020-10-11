41a593abaae6d9c248994d3aa4741c41c8501a45
Artur Symanovic <artur.symanovic@gmail.com>
2020-10-11 20:45:11 +0100

    Added member messages component

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -107,10 +107,18 @@ export class UserService {
         map((response) => {
           paginatedResult.result = response.body;
           if (response.headers.get('Pagination') != null) {
-            paginatedResult.pagination = JSON.parse(response.headers.get('Pagination'));
+            paginatedResult.pagination = JSON.parse(
+              response.headers.get('Pagination')
+            );
           }
           return paginatedResult;
         })
       );
+  }
+
+  getMessageThread(id: number, recipientId: number): any {
+    return this.http.get<Message[]>(
+      this.baseUrl + 'users/' + id + '/messages/thread/' + recipientId
+    );
   }
 }
